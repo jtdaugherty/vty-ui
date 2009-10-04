e0870bbb228a4f0867df0147441cc996e1ca08d9
drcygnus <drcygnus@gmail.com>
2009-10-03 22:09:49 -0700

    List: expose listItems
    Ignore-this: bd3656aa82e0a227e9f6ffa8c4aaef74
    
    darcs-hash:20091004050949-e018d-23073df6f4b118168654bddaab031f900a638c16.gz

--- src/Graphics/Vty/Widgets/List.hs
+++ src/Graphics/Vty/Widgets/List.hs
@@ -18,6 +18,7 @@ module Graphics.Vty.Widgets.List
     , scrollDown
     , scrollUp
     -- ** List inspection
+    , listItems
     , getSelected
     , selectedIndex
     , scrollTopIndex
@@ -38,7 +39,7 @@ data List = List { normalAttr :: Attr
                  , selectedIndex :: Int -- ^The currently selected list index.
                  , scrollTopIndex :: Int -- ^The start index of the window of visible list items.
                  , scrollWindowSize :: Int -- ^The size of the window of visible list items.
-                 , listItems :: [String]
+                 , listItems :: [String] -- ^The items in the list.
                  }
 
 -- |Create a new list.  Emtpy lists are not allowed.
