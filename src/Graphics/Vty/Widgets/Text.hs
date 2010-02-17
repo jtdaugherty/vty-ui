870ec4b23ca70e403de1f30189d692e724c2f4ae
drcygnus <drcygnus@gmail.com>
2010-02-16 20:58:21 -0800

    Graphics.Vty.Widgets.Text: haddock
    Ignore-this: dddb6cf24906ec79e13bfcd6320ed225
    
    darcs-hash:20100217045821-e018d-a1068067beed5b9411d0f6f7e5b9fc5ce93143a7.gz

--- src/Graphics/Vty/Widgets/Text.hs
+++ src/Graphics/Vty/Widgets/Text.hs
@@ -1,11 +1,20 @@
+-- |This module provides functionality for rendering 'String's as
+-- 'Widget's, including functionality to make structural and/or visual
+-- changes at rendering time.  To get started, turn your ordinary
+-- 'String' into a 'Widget' with 'simpleText'; if you want access to
+-- the 'Text' for formatting purposes, use 'prepareText' followed by
+-- 'textWidget'.
 module Graphics.Vty.Widgets.Text
-    ( Formatter
-    , Text(defaultAttr, tokens)
-    , wrap
-    , simpleText
+    ( Text(defaultAttr, tokens)
+    -- *Text Preparation
     , prepareText
+    -- *Constructing Widgets
+    , simpleText
     , textWidget
+    -- *Formatting
+    , Formatter
     , highlight
+    , wrap
     )
 where
 
@@ -45,19 +54,39 @@ import Text.Regex.PCRE.Light.Char8
     , exec_anchored
     )
 
+-- |A formatter makes changes to text at rendering time.
+--
+-- It'd be nice if formatters were just @:: 'Text' -> 'Text'@, but
+-- some formatting use cases involve knowing the size of the rendering
+-- area, which is not known until render time (e.g., text wrapping).
+-- Thus, a formatter takes a 'DisplayRegion' and runs at render time.
 type Formatter = DisplayRegion -> Text -> Text
+
+-- |Text represents a String that can be manipulated with 'Formatter's
+-- at rendering time.
 data Text = Text { defaultAttr :: Attr
+                 -- ^The default attribute for all tokens in this text
+                 -- object.
                  , tokens :: [Token Attr]
+                 -- ^The tokens of the underlying text stream.
                  }
 
+-- |Given a default attribute, turn a String into Text.
 prepareText :: Attr -> String -> Text
 prepareText att s = Text { defaultAttr = att
                          , tokens = tokenize s att
                          }
 
+-- |Construct a Widget directly from an attribute and a String.  This
+-- is recommended if you don't have any special formatting
+-- requirements.
 simpleText :: Attr -> String -> Widget
 simpleText a s = textWidget [] $ prepareText a s
 
+-- |A formatter for wrapping text into the available space (known at
+-- rendering time).  This formatter will insert line breaks where
+-- appropriate, so if you want to use other structure-sensitive
+-- formatters, run this formatter last.
 wrap :: Formatter
 wrap sz t = t { tokens = newTokens }
     where
@@ -74,13 +103,19 @@ highlight :: Regex -> Attr -> Formatter
 highlight regex attr =
     \_ t -> t { tokens = map (annotate (matchesRegex regex) attr) $ tokens t }
 
+-- |Possibly annotate a token with the specified annotation value if
+-- the predicate matches; otherwise, return the input token unchanged.
 annotate :: (Token a -> Bool) -> a -> Token a -> Token a
 annotate f ann t = if f t then t `withAnnotation` ann else t
 
+-- |Does the specified regex match the token's string value?
 matchesRegex :: Regex -> Token a -> Bool
 matchesRegex r (Token s _) = isJust $ match r s [exec_anchored]
 matchesRegex _ _ = False
 
+-- |Construct a text widget formatted with the specified formatters.
+-- the formatters will be applied in the order given here, so be aware
+-- of how the formatters will modify the text (and affect each other).
 textWidget :: [Formatter] -> Text -> Widget
 textWidget formatters t = Widget {
                             growHorizontal = False
@@ -93,10 +128,13 @@ textWidget formatters t = Widget {
     where
       newText att = t { tokens = map (\c -> withAnnotation c att) $ tokens t }
 
+-- |Given formatters, apply the formatters to the specified text
+-- stream using the given rendering region.
 applyFormatters :: [Formatter] -> DisplayRegion -> Text -> Text
 applyFormatters [] _ t = t
 applyFormatters (f:fs) sz t = applyFormatters fs sz $ f sz t
 
+-- |Low-level text-rendering routine.
 renderText :: Text -> [Formatter] -> DisplayRegion -> Render
 renderText t formatters sz =
     if region_height sz == 0
