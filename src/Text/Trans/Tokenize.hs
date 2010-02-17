051b6885cd42ea6fbfc5fff92f8a081d3ccf2a7b
drcygnus <drcygnus@gmail.com>
2010-02-16 20:01:28 -0800

    Text.Trans.Tokenize: haddock
    Ignore-this: 1fe622582167cc7badc2efc625c4b473
    
    darcs-hash:20100217040128-e018d-271d11eb224b22369b6a5b352775a1827654fbe4.gz

--- src/Text/Trans/Tokenize.hs
+++ src/Text/Trans/Tokenize.hs
@@ -1,3 +1,12 @@
+-- |This module provides a tokenization API for text strings.  The
+-- idea is that if you want to make structural or representational
+-- changes to a text stream, it needs to be split up into reasonable
+-- tokens first, with structural properties intact.  This is
+-- accomplished by the 'Token' type.  To get started, call 'tokenize'
+-- to turn your String into tokens; then you can use the other
+-- operations provided here to make structural or representational
+-- changes.  To serialize a token list to its underlying string form,
+-- use 'serialize'.
 module Text.Trans.Tokenize
     ( Token(..)
     , tokenize
@@ -18,11 +27,20 @@ import Data.List
     , splitAt
     )
 
+-- |The type of textual tokens.  Tokens have an "annotation" type,
+-- which is the type of values that can be used to annotate tokens
+-- (e.g., position in a file, visual attributes, etc.).
 data Token a = Newline a
+             -- ^A line break.
              | Whitespace String a
+             -- ^A sequence of whitespace characters.
              | Token String a
+             -- ^A non-whitespace token.
                deriving (Show, Eq)
 
+-- |General splitter function; given a list and a predicate, split the
+-- list into sublists wherever the predicate matches, discarding the
+-- matching elements.
 splitWith :: (Eq a) => [a] -> (a -> Bool) -> [[a]]
 splitWith [] _ = []
 splitWith es f = if null rest
@@ -37,6 +55,7 @@ wsChars = [' ', '\t']
 isWs :: Char -> Bool
 isWs = (`elem` wsChars)
 
+-- |Tokenize a string using a default annotation value.
 tokenize :: String -> a -> [Token a]
 tokenize [] _ = []
 tokenize ('\n':rest) a = Newline a : tokenize rest a
@@ -47,25 +66,32 @@ tokenize s a = Token t a : tokenize rest a
     where
       (t, rest) = break (\c -> isWs c || c == '\n') s
 
+-- |Serialize tokens to an underlying string representation,
+-- discarding annotations.
 serialize :: [Token a] -> String
 serialize [] = []
 serialize (Newline _:rest) = "\n" ++ serialize rest
 serialize (Whitespace s _:rest) = s ++ serialize rest
 serialize (Token s _:rest) = s ++ serialize rest
 
+-- |Replace a token's annotation.
 withAnnotation :: Token a -> a -> Token a
 withAnnotation (Newline _) b = Newline b
 withAnnotation (Whitespace s _) b = Whitespace s b
 withAnnotation (Token s _) b = Token s b
 
+-- |Is the token a line break?
 isNewline :: Token a -> Bool
 isNewline (Newline _) = True
 isNewline _ = False
 
+-- |Is the token whitespace?
 isWhitespace :: Token a -> Bool
 isWhitespace (Whitespace _ _) = True
 isWhitespace _ = False
 
+-- |Split a list of tokens at Newlines, such that the returned token
+-- lists do not contain Newlines.
 splitLines :: (Eq a) => [Token a] -> [[Token a]]
 splitLines ts = splitWith ts isNewline
 
@@ -75,6 +101,10 @@ trunc def ts width = concat $ intersperse [Newline def] newLines
     where
       newLines = map (\l -> truncLine width l) $ splitLines ts
 
+-- |Given a list of tokens, truncate the list so that its underlying
+-- string representation does not exceed the specified column width.
+-- Note that this does not truncate /within/ a token; it merely
+-- returns the largest sublist of tokens that has the required length.
 truncLine :: Int -> [Token a] -> [Token a]
 truncLine width ts = take (length $ head passing) ts
     where
@@ -89,6 +119,9 @@ len (Token s _) = length s
 
 -- XXX This assumes that the input token list will not contain
 -- newlines (i.e., that splitLines has already been called).
+-- |Given a list of tokens without Newlines, (potentially) wrap the
+-- list to the specified column width, using the specified default
+-- annotation.
 wrapLine :: (Eq a) => a -> Int -> [Token a] -> [[Token a]]
 wrapLine _ _ [] = []
 wrapLine def width ts =
