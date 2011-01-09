2052365fbddb7dc9fda4c76f1f9f8a9913216fd3
drcygnus <drcygnus@gmail.com>
2011-01-08 22:19:56 -0800

    Demo2: tweaks
    Ignore-this: 9d363f474e6655d7a1389d1959e6e8d0
    
    darcs-hash:20110109061956-e018d-915bd43d91fff2387735ea1396ba308c2eceb771.gz

--- src/Demo2.hs
+++ src/Demo2.hs
@@ -6,11 +6,12 @@ import Graphics.Vty
 import Graphics.Vty.Widgets.All
 
 -- Visual attributes.
-titleAttr = bright_white `on` blue
+borderAttr = blue `on` black
 editAttr = white `on` black
-editFocusAttr = bright_green `on` black
-bodyAttr = bright_green `on` black
+editFocusAttr = bright_yellow `on` blue
+bodyAttr = white `on` black
 headerAttr = bright_yellow `on` black
+msgAttr = bright_white `on` black
 
 exitApp :: Vty -> IO a
 exitApp vty = do
@@ -22,15 +23,17 @@ main :: IO ()
 main = do
   vty <- mkVty
 
-  edit1 <- editWidget editAttr editFocusAttr
-  edit2 <- editWidget editAttr editFocusAttr
+  let msg = "Press <TAB> to switch edit fields; press <ESC> to quit."
 
-  table <- newTable bodyAttr [Fixed 20, Fixed 20] BorderFull
-  ui <- centered table
+  table <- newTable borderAttr [Fixed 20, Fixed 20] BorderFull
+  ui <- centered =<< (return table
+                      <--> (hLimit 43 =<< (textWidget wrap $ prepareText msgAttr msg)))
 
   [col1Header, col2Header] <-
       addHeadingRow table headerAttr ["", ""]
 
+  edit1 <- editWidget editAttr editFocusAttr
+  edit2 <- editWidget editAttr editFocusAttr
   addRow table [ mkCell edit1, mkCell edit2 ]
 
   edit1 `onChange` \_ s -> setText col1Header headerAttr s
@@ -40,7 +43,6 @@ main = do
   setEditText edit2 "Bar"
 
   fg <- newFocusGroup
-
   fg `onKeyPressed` \_ k _ -> do
          case k of
            KEsc -> exitApp vty
