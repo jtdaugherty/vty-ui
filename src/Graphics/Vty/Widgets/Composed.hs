44b3c5f028c8385b028074e18a5b45f09741922d
drcygnus <drcygnus@gmail.com>
2011-01-15 23:45:23 -0800

    Composed: put widgets into MonadIO
    Ignore-this: d7e5a6cfc11c0312beadd1299d15d78f
    
    darcs-hash:20110116074523-e018d-b00aeb752859e3459b77e2991c1fc822dff00790.gz

--- src/Graphics/Vty/Widgets/Composed.hs
+++ src/Graphics/Vty/Widgets/Composed.hs
@@ -6,6 +6,9 @@ module Graphics.Vty.Widgets.Composed
     )
 where
 
+import Control.Monad.Trans
+    ( MonadIO
+    )
 import Graphics.Vty
     ( Attr
     )
@@ -24,22 +27,23 @@ import Graphics.Vty.Widgets.Base
     )
 
 -- |Add expanding bottom padding to a widget.
-bottomPadded :: Widget a -> Attr -> IO (Widget (Box a VFill))
+bottomPadded :: (MonadIO m) => Widget a -> Attr -> m (Widget (Box a VFill))
 bottomPadded w attr = do
   f <- vFill attr ' '
   vBox w f
 
 -- |Add expanding top padding to a widget.
-topPadded :: Widget a -> Attr -> IO (Widget (Box VFill a))
+topPadded :: (MonadIO m) => Widget a -> Attr -> m (Widget (Box VFill a))
 topPadded w attr = do
   f <- vFill attr ' '
   vBox f w
 
 -- |Impose a maximum size (width, height) on a widget.
-boxLimit :: Int -- ^Maximum width in columns
+boxLimit :: (MonadIO m) =>
+            Int -- ^Maximum width in columns
          -> Int -- ^Maximum height in rows
          -> Widget a
-         -> IO (Widget (VLimit (HLimit a)))
+         -> m (Widget (VLimit (HLimit a)))
 boxLimit maxWidth maxHeight w = do
   ch <- hLimit maxWidth w
   vLimit maxHeight ch
