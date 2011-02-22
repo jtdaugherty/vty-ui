c9eb5ec32ff664d03f0fc25d4765f3afb32b9ead
drcygnus <drcygnus@gmail.com>
2011-02-21 19:59:10 -0800

    Manual updates
    Ignore-this: 867e01644f3416b6594f874e7e736c54
    
    darcs-hash:20110222035910-e018d-eebc916958b5a5b4efacc20700e26967ee186cea.gz

--- src/Demo3.hs
+++ src/Demo3.hs
@@ -1,32 +1,19 @@
 module Main where
 
-import System.Posix.Files
-import Data.List
-import Graphics.Vty
 import Graphics.Vty.Widgets.All
-
-customAnnotations :: [( FilePath -> FileStatus -> Bool
-                      , FilePath -> FileStatus -> IO String
-                      , Attr)]
-customAnnotations = [ (\p _ -> "~" `isSuffixOf` p
-                      , \_ _ -> return "emacs backup file"
-                      , yellow `on` blue)
-                    ]
 
 main :: IO ()
 main = do
-  (b, fg) <- newDirBrowser $ defaultBrowserSkin
-             `withAnnotations` customAnnotations
+  e <- editWidget
+  ui <- centered e
 
-  b `onBrowseAccept` \p ->
-      if "~" `isSuffixOf` p
-      then error $ "You chose: " ++ p
-      else reportBrowserError b "Please select an emacs backup file."
-
-  b `onBrowseCancel` const (error "Cancelled.")
+  fg <- newFocusGroup
+  addToFocusGroup fg e
 
   c <- newCollection
-  _ <- addToCollection c (dirBrowserWidget b) fg
+  addToCollection c ui fg
 
-  runUi c $ defaultContext { focusAttr = (black `on` yellow)
-                           }
+  e `onActivate` \this ->
+      getEditText this >>= (error . ("You entered: " ++))
+
+  runUi c defaultContext
