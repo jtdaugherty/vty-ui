f51a3907cd5ce3d4d4874034d4cbaa7110b37881
drcygnus <drcygnus@gmail.com>
2011-03-01 22:23:38 -0800

    CheckBox module API docs
    Ignore-this: a9875af33104e84405894d9400c97ae5
    
    darcs-hash:20110302062338-e018d-ef58625980dbf76f5543fa11a60c2bdbf71681f4.gz

--- src/Graphics/Vty/Widgets/CheckBox.hs
+++ src/Graphics/Vty/Widgets/CheckBox.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, DeriveDataTypeable #-}
+-- |This module provides ''check box'' widgets and ''radio button''
+-- widgets.  In addition, this module provides a generalized
+-- ''multi-state'' check box type which allows you to set multiple
+-- states in the checkbox, each with its own character
+-- representation.
+--
+-- All of these types of widgets are toggled with the Spacebar and
+-- Enter keys.
 module Graphics.Vty.Widgets.CheckBox
     ( CheckBox
     , RadioGroup
@@ -47,20 +55,28 @@ data RadioGroupData = RadioGroupData { currentlySelected :: Maybe (Widget (Check
 
 type RadioGroup = IORef RadioGroupData
 
+-- |Create a new radio button group.  This is used to guarantee
+-- exclusivity among the check boxes in the group so that they behave
+-- like radio buttons.
 newRadioGroup :: (MonadIO m) => m RadioGroup
 newRadioGroup = do
   hs <- newHandlers
   liftIO $ newIORef $ RadioGroupData Nothing hs
 
+-- |Register a handler to be notified when the currently-selected
+-- check box in a radio group changes.
 onRadioChange :: (MonadIO m) => RadioGroup -> (Widget (CheckBox Bool) -> IO ())
               -> m ()
 onRadioChange rg act = do
   rd <- liftIO $ readIORef rg
   addHandler (return . changeHandlers) rd act
 
+-- |Get the currently-selected checkbox in a radio group, if any.
 getCurrentRadio :: (MonadIO m) => RadioGroup -> m (Maybe (Widget (CheckBox Bool)))
 getCurrentRadio = (currentlySelected <~)
 
+-- |Add a check box to a radio group.  The check box's apperance will
+-- be changed so that it resembles a radio button.
 addToRadioGroup :: (MonadIO m) => RadioGroup -> Widget (CheckBox Bool) -> m ()
 addToRadioGroup rg wRef = do
   setStateChar wRef True '*'
@@ -93,8 +109,16 @@ addToRadioGroup rg wRef = do
         writeIORef rg $ rgData { currentlySelected = Just wRef }
 
 data CheckBoxError = EmptyCheckboxStates
+                   -- ^Indicates that an empty state list was used to
+                   -- create a multi-state checkbox.
                    | BadCheckboxState
+                   -- ^Indicates that a checkbox state value is not a
+                   -- valid state value in the checkbox's state
+                   -- mapping.
                    | BadStateArgument
+                     -- ^Indicates that a state argument used for a
+                     -- checkbox state transition is not a valid state
+                     -- for the checkbox.
                      deriving (Show, Typeable)
 
 instance Exception CheckBoxError
@@ -117,10 +141,18 @@ instance Show a => Show (CheckBox a) where
                      , " }"
                      ]
 
+-- |Create a new checkbox with the specified text label.
 newCheckbox :: (MonadIO m) => String -> m (Widget (CheckBox Bool))
 newCheckbox label = newMultiStateCheckbox label [(False, ' '), (True, 'x')]
 
-newMultiStateCheckbox :: (Eq a, MonadIO m) => String -> [(a, Char)] -> m (Widget (CheckBox a))
+-- |Create a new multi-state checkbox.
+newMultiStateCheckbox :: (Eq a, MonadIO m) =>
+                         String -- ^The checkbox label.
+                      -> [(a, Char)] -- ^The list of valid states that
+                                     -- the checkbox can be in, along
+                                     -- with the visual representation
+                                     -- ('Char') for each state.
+                      -> m (Widget (CheckBox a))
 newMultiStateCheckbox _ [] = throw EmptyCheckboxStates
 newMultiStateCheckbox label states = do
   cchs <- newHandlers
@@ -168,6 +200,8 @@ modifyElem as i f = concat [ take i as
                            , drop (i + 1) as
                            ]
 
+-- |Set the visual representation for a state in a checkbox.  May
+-- throw 'BadStateArgument'.
 setStateChar :: (Eq a, MonadIO m) => Widget (CheckBox a) -> a -> Char -> m ()
 setStateChar wRef v ch = do
   states <- checkboxStates <~~ wRef
@@ -180,12 +214,15 @@ setStateChar wRef v ch = do
 
   updateWidgetState wRef $ \s -> s { checkboxStates = newStates }
 
+-- |Set the checkbox's bracketing characters for the left and right
+-- brackets around the state character.
 setBracketChars :: (MonadIO m) => Widget (CheckBox a) -> Char -> Char -> m ()
 setBracketChars wRef chL chR =
     updateWidgetState wRef $ \s -> s { leftBracketChar = chL
                                      , rightBracketChar = chR
                                      }
 
+-- |Get a checkbox's text label.
 getCheckboxLabel :: (MonadIO m) => Widget (CheckBox a) -> m String
 getCheckboxLabel = (checkboxLabel <~~)
 
@@ -194,23 +231,29 @@ radioKeyEvent this (KASCII ' ') [] = cycleCheckbox this >> return True
 radioKeyEvent this KEnter [] = cycleCheckbox this >> return True
 radioKeyEvent _ _ _ = return False
 
+-- |Set the state of a checkbox.  May throw 'BadCheckboxState'.
 setCheckboxState :: (Eq a, MonadIO m) => Widget (CheckBox a) -> a -> m ()
 setCheckboxState = setChecked_
 
+-- |Set a binary checkbox to unchecked.
 setCheckboxUnchecked :: (MonadIO m) => Widget (CheckBox Bool) -> m ()
 setCheckboxUnchecked wRef = setCheckboxState wRef False
 
+-- |Set a binary checkbox to checked.
 setCheckboxChecked :: (MonadIO m) => Widget (CheckBox Bool) -> m ()
 setCheckboxChecked wRef = setCheckboxState wRef True
 
+-- |Toggle a binary checkbox.
 toggleCheckbox :: (MonadIO m) => Widget (CheckBox Bool) -> m ()
 toggleCheckbox wRef = do
   v <- currentState <~~ wRef
   setCheckboxState wRef (not v)
 
+-- |Get a checkbox's current state value.
 getCheckboxState :: (MonadIO m) => Widget (CheckBox a) -> m a
 getCheckboxState = (currentState <~~)
 
+-- |Cycle a checkbox's state to the next value in its state list.
 cycleCheckbox :: (Eq a, MonadIO m) => Widget (CheckBox a) -> m ()
 cycleCheckbox wRef = do
   v <- currentState <~~ wRef
@@ -240,6 +283,8 @@ notifyChangeHandlers wRef = do
   v <- currentState <~~ wRef
   fireEvent wRef (checkboxChangeHandlers <~~) v
 
+-- |Register a handler for a checkbox state change.  The handler will
+-- be passed the new state value.
 onCheckboxChange :: (MonadIO m) => Widget (CheckBox a) -> (a -> IO ()) -> m ()
 onCheckboxChange = addHandler (checkboxChangeHandlers <~~)
 
