0f11ef05550f75a3c8a73384e934412609921add
drcygnus <drcygnus@gmail.com>
2011-03-02 21:42:42 -0800

    Events module API docs
    Ignore-this: 5e58537c206a547ef0ce7beb23a181cb
    
    darcs-hash:20110303054242-e018d-13bcdf4bed0eeeac02250cb931b1de98d4c10687.gz

--- src/Graphics/Vty/Widgets/Events.hs
+++ src/Graphics/Vty/Widgets/Events.hs
@@ -1,3 +1,6 @@
+-- |This module provides infrastructure for widgets that need to
+-- produce events and provide event handler registration
+-- functionality.
 module Graphics.Vty.Widgets.Events
     ( Handlers
     , Handler
@@ -11,14 +14,23 @@ import Control.Monad.Trans
 import Control.Monad
 import Data.IORef
 
+-- |The type of event handlers which take a parameter of type 'a'.
 type Handler a = a -> IO ()
+
+-- |The type of event handler collections of parameter type 'a'.
 newtype Handlers a = Handlers (IORef [Handler a])
 
+-- |Given an event handler collection projection combinator, a target,
+-- and a handler, add the handler to the target's event handler
+-- collection.
 addHandler :: (MonadIO m) => (w -> m (Handlers a)) -> w -> Handler a -> m ()
 addHandler getRef w handler = do
   (Handlers r) <- getRef w
   liftIO $ modifyIORef r $ \s -> s ++ [handler]
 
+-- |Fire an event by extracting an event handler collection from a
+-- target and invoking all of its handlers with the specified
+-- parameter value.
 fireEvent :: (MonadIO m) => w -> (w -> m (Handlers a)) -> a -> m ()
 fireEvent w getRef ev = do
   (Handlers r) <- getRef w
@@ -26,6 +38,7 @@ fireEvent w getRef ev = do
   forM_ handlers $ \handler ->
       liftIO $ handler ev
 
+-- |Create a new event handler collection.
 newHandlers :: (MonadIO m) => m (Handlers a)
 newHandlers = do
   r <- liftIO $ newIORef []
