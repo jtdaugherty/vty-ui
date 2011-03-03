c145958ebe2a7cbe3e17876632384d4ecdba0c4d
drcygnus <drcygnus@gmail.com>
2011-03-02 21:37:57 -0800

    ProgressBar module API docs
    Ignore-this: fa751de626d3f6ba39c8027a2964acc4
    
    darcs-hash:20110303053757-e018d-c02046444c33e0fd143a9dc768fdfbfec8694438.gz

--- src/Graphics/Vty/Widgets/ProgressBar.hs
+++ src/Graphics/Vty/Widgets/ProgressBar.hs
@@ -1,6 +1,10 @@
+-- |This module provides a ''progress bar'' widget which stores a
+-- progress value between 0 and 100 inclusive.  Use the 'schedule'
+-- function to modify the progress bar's state from a thread.
 module Graphics.Vty.Widgets.ProgressBar
-    ( ProgressBar(progressBarWidget)
+    ( ProgressBar
     , newProgressBar
+    , progressBarWidget
     , setProgress
     , addProgress
     , getProgress
@@ -19,10 +23,13 @@ import Graphics.Vty.Widgets.Events
 import Graphics.Vty.Widgets.Util
 
 data ProgressBar = ProgressBar { progressBarWidget :: Widget (Box HFill HFill)
+                               -- ^Get the widget of a progress bar.
                                , progressBarAmount :: IORef Int
                                , onChangeHandlers :: Handlers Int
                                }
 
+-- |Create a new progress bar with the specified completed and
+-- uncompleted colors, respectively.
 newProgressBar :: (MonadIO m) => Color -> Color -> m ProgressBar
 newProgressBar completeColor incompleteColor = do
   let completeAttr = completeColor `on` completeColor
@@ -36,18 +43,24 @@ newProgressBar completeColor incompleteColor = do
   setProgress p 0
   return p
 
+-- |Register a handler to be invoked when the progress bar's progress
+-- value changes.  The handler will be passed the new progress value.
 onProgressChange :: (MonadIO m) => ProgressBar -> (Int -> IO ()) -> m ()
 onProgressChange = addHandler (return . onChangeHandlers)
 
+-- |Set the progress bar's progress value.  Values outside the allowed
+-- range will be ignored.
 setProgress :: (MonadIO m) => ProgressBar -> Int -> m ()
 setProgress p amt = do
   liftIO $ writeIORef (progressBarAmount p) amt
   setBoxChildSizePolicy (progressBarWidget p) $ Percentage amt
   fireEvent p (return . onChangeHandlers) amt
 
+-- |Get the progress bar's current progress value.
 getProgress :: (MonadIO m) => ProgressBar -> m Int
 getProgress = liftIO . readIORef . progressBarAmount
 
+-- |Add a delta value to the progress bar's current value.
 addProgress :: (MonadIO m) => ProgressBar -> Int -> m ()
 addProgress p amt = do
   cur <- getProgress p
