f4b38cce24b4e4825eff8eb07f9ed58179b87843
Jonathan Daugherty <jtd@galois.com>
2012-03-18 19:26:52 -0700

    Haddock improvements for Alignment module

--- src/Graphics/Vty/Widgets/Alignment.hs
+++ src/Graphics/Vty/Widgets/Alignment.hs
@@ -1,3 +1,6 @@
+-- |This module provides a type and a type class for expressing
+-- alignment.  For concrete uses, see the Table and ProgressBar
+-- modules.
 module Graphics.Vty.Widgets.Alignment
     ( Alignment(..)
     , Alignable(..)
@@ -8,6 +11,6 @@ where
 data Alignment = AlignCenter | AlignLeft | AlignRight
                  deriving (Show)
 
--- |The class of types whose values can be aligned.
+-- |The class of types whose values or contents can be aligned.
 class Alignable a where
     align :: a -> Alignment -> a
